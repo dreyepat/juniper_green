9faf5db09189f9d33adbfe793044c4775e434ea7
Simone Sterren <simster@hotmail.de>
2013-05-28 15:32:39 +0200

    nicht benoetigte files korrekt geloescht

--- src/test/java/ch/sipama/Controller/CompHoechstTest.java
+++ src/test/java/ch/sipama/Controller/CompHoechstTest.java
@@ -1,20 +1,32 @@
 package ch.sipama.Controller;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
 
 public class CompHoechstTest {
 	
 	private Spieldaten oSpdaten;
-
-	@Test
-	public void testCompHoechst() {
-		assertNotNull("Spieldaten", Spieldaten.getInstance());
+	
+	public void testAll() {
+		oSpdaten = Spieldaten.getInstance();
+		oSpdaten.setSpieldaten(10, "sSpielerA", "sSpielerB");
+		oSpdaten.spielzugAusfuehren(0, 1, 2);
+		
+		//Objekt erstellen und testen, ob Erstellung klappte
+		CompHoechst ch = new CompHoechst();
+		assertNotNull(ch);
+		
+		//PC-Spielzug ausführen und prüfen, ob grösst mögliche Zahl gezogen wurde
+		assertEquals("Höchst mögliche Zahl ist 10", 10, ch.naechsterPCSpielzug());
+		oSpdaten.spielzugAusfuehren(0, 1, 5);
+		assertEquals("Höchst mögliche Zahl ist 1", 1, ch.naechsterPCSpielzug());
+		
+		//PC-Spielzug ausführen und prüfen, ob grösste Primzahl zurückgegeben wird
+		oSpdaten.spielzugAusfuehren(0, 0, 1);
+		assertEquals("Nach gezogener 1 wird die höchste Primzahl gezogen", 7, ch.naechsterPCSpielzug());
+		
+//		for (int i = 0; i < 1; i++) {
+//			if (false) Assert.fail("array fails");
+//		}
+		
 	}
-
-	@Test
-	public void testNaechsterPCSpielzug() {
-		assertNotNull("Spielzuege", 0);
-	}
-
 }
