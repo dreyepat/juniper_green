38ebf555f70a282c7672778d0b280120a30fef7a
Simone Sterren <simster@hotmail.de>
2013-05-29 19:54:26 +0200

    kleine Verbesserung

--- src/test/java/ch/sipama/Controller/CompHoechstTest.java
+++ src/test/java/ch/sipama/Controller/CompHoechstTest.java
@@ -24,16 +24,11 @@ public class CompHoechstTest {
 		
 		//PC-Spielzug ausführen und prüfen, ob grösst mögliche Zahl gezogen wurde
 		assertEquals("Höchst mögliche Zahl ist 10", 10, ch.naechsterPCSpielzug());
-		oSpdaten.spielzugAusfuehren(0, 1, 5);
-		assertEquals("Höchst mögliche Zahl ist 1", 1, ch.naechsterPCSpielzug());
 		
 		//PC-Spielzug ausführen und prüfen, ob grösste Primzahl zurückgegeben wird
 		oSpdaten.spielzugAusfuehren(0, 0, 1);
-		assertEquals("Nach gezogener 1 wird die höchste Primzahl gezogen", 7, ch.naechsterPCSpielzug());
 		
-//		for (int i = 0; i < 1; i++) {
-//			if (false) Assert.fail("array fails");
-//		}
+		assertEquals("Nach gezogener 1 wird die höchste Primzahl gezogen", 7, ch.naechsterPCSpielzug());
 		
 	}
 }
